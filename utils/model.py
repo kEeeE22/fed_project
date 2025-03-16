91a1417e32c5eea84fe12dcca8d98562b403e718
Hai <danghoanghai042@gmail.com>
2025-03-16 13:42:46 +0700

    abc

--- utils/model.py
+++ utils/model.py
@@ -138,3 +138,29 @@ class ETC_CNN2(nn.Module):
         x = self.dropout(x)
         x = self.fc2(x)
         return x
+
+class ETC_CNN3(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ETC_CNN3, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Tính toán kích thước sau khi qua các lớp pooling
+        self.fc1 = nn.Linear(128 * 2 * 16, 256)  # Đầu vào 20x64 -> sau 3 lần pooling còn 2x16
+        self.fc2 = nn.Linear(256, num_classes)
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = x.view(x.size(0), -1)  # Flatten
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
