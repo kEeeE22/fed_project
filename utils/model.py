7ca71dfb7cd21e016985572429c10c8fc9332bd3
Hai <danghoanghai042@gmail.com>
2025-03-23 18:12:37 +0700

    add dropout

--- utils/model.py
+++ utils/model.py
@@ -150,24 +150,34 @@ class ETC_CNN3(nn.Module):
         super(ETC_CNN3, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
+
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
+        self.drop1 = nn.Dropout(0.3)  # Dropout sau conv2
+
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(128)
+        self.drop2 = nn.Dropout(0.3)  # Dropout sau conv3
+
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        
+
         self.fc1 = nn.Linear(2048, 256)  # Đầu vào 20x64 -> sau 3 lần pooling còn 2x16
+        self.bn_fc = nn.BatchNorm1d(256)
+        self.drop_fc = nn.Dropout(0.5)  # Dropout trước FC2
         self.fc2 = nn.Linear(256, num_classes)
-        self.dropout = nn.Dropout(0.5)
         self.bic = BiCLayer(num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.bn1(self.conv1(x))))
         x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.drop1(x)  # Áp dụng Dropout sau MaxPool
+
         x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.drop2(x)  # Áp dụng Dropout sau MaxPool
+
         x = x.view(x.size(0), -1)  # Flatten
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
+        x = F.relu(self.bn_fc(self.fc1(x)))
+        x = self.drop_fc(x)  # Áp dụng Dropout trước FC2
         x = self.fc2(x)
         x = self.bic(x)
         return x
@@ -191,40 +201,45 @@ class ETC_RESNET18(nn.Module):
         return x
     
 class ETC_CNN1D(nn.Module):
-    def __init__(self,input_channels=1, output_size=3):
+    def __init__(self, input_channels=1, output_size=3):
         super(ETC_CNN1D, self).__init__()
-        
+
         self.conv1 = nn.Conv1d(in_channels=input_channels, out_channels=64, kernel_size=5, stride=1, padding=2)
-        self.bn1 = nn.BatchNorm1d(64)  # BatchNorm sau conv1
+        self.bn1 = nn.BatchNorm1d(64)
 
         self.conv2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=5, stride=1, padding=2)
-        self.bn2 = nn.BatchNorm1d(128)  # BatchNorm sau conv2
+        self.bn2 = nn.BatchNorm1d(128)
         self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.drop1 = nn.Dropout(0.3)  # Dropout sau conv2
 
         self.conv3 = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm1d(256)  # BatchNorm sau conv3
+        self.bn3 = nn.BatchNorm1d(256)
 
         self.conv4 = nn.Conv1d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
-        self.bn4 = nn.BatchNorm1d(512)  # BatchNorm sau conv4
+        self.bn4 = nn.BatchNorm1d(512)
+        self.drop2 = nn.Dropout(0.3)  # Dropout sau conv4
 
-        # Tính toán kích thước đầu vào của FC
         final_feature_map_size = 1280 // 4  # Sau 2 lớp MaxPool1d
         self.fc1 = nn.Linear(512 * final_feature_map_size, 256)
-        self.bn_fc = nn.BatchNorm1d(256)  # BatchNorm trước FC2
+        self.bn_fc = nn.BatchNorm1d(256)
+        self.drop_fc = nn.Dropout(0.5)  # Dropout trước FC2
         self.fc2 = nn.Linear(256, output_size)
         self.bic = BiCLayer(output_size)
 
     def forward(self, x):
-        x = x.view(x.size(0), 1, -1)  # Chuyển (batch, 1, 20, 64) → (batch, 1, 1280)
+        x = x.view(x.size(0), 1, -1)
 
         x = F.relu(self.bn1(self.conv1(x)))
         x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.drop1(x)  # Áp dụng Dropout sau MaxPool1d
 
         x = F.relu(self.bn3(self.conv3(x)))
         x = self.pool(F.relu(self.bn4(self.conv4(x))))
+        x = self.drop2(x)  # Áp dụng Dropout sau MaxPool1d
 
-        x = x.view(x.size(0), -1)  # Flatten
+        x = x.view(x.size(0), -1)
         x = F.relu(self.bn_fc(self.fc1(x)))
+        x = self.drop_fc(x)  # Áp dụng Dropout trước FC2
         x = self.fc2(x)
         x = self.bic(x)
         return x
