aa8cd3d0d2ff272d605cd41013abf118690bc255
Hai <danghoanghai042@gmail.com>
2025-03-23 14:30:04 +0700

    asdf

--- utils/model.py
+++ utils/model.py
@@ -191,20 +191,27 @@ class ETC_RESNET18(nn.Module):
         return x
     
 class ETC_CNN1D(nn.Module):
-    def __init__(self, input_channels=1, output_size=3):
+    def __init__(self,input_channels=1, output_size=3):
         super(ETC_CNN1D, self).__init__()
         
-        self.conv1 = nn.Conv1d(in_channels=input_channels, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv1d(in_channels=input_channels, out_channels=64, kernel_size=5, stride=1, padding=2)
+        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=5, stride=1, padding=2)
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
         
-        self.fc1 = nn.Linear(64 * 20, 128) 
-        self.fc2 = nn.Linear(128, output_size)
+        self.conv3 = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv1d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
+        
+        self.fc1 = nn.Linear(512 * (20*64 // 16), 256)
+        self.fc2 = nn.Linear(256, output_size)
         self.bic = BiCLayer(output_size)
 
     def forward(self, x):
-        x = x.squeeze(1)
+        x = x.view(x.size(0), 1, -1)
         x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        
+        x = F.relu(self.conv3(x))
+        x = self.pool(F.relu(self.conv4(x)))
         
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
